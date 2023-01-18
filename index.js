139630c8d726e87b30fc00394180c70008d43eb1
Gilbert Malova <malovagilbert@gmail.com>
2023-01-18 19:29:04 +0200

    Final solution

--- index.js
+++ index.js
@@ -2,4 +2,58 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
-console.log(999);
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : path.basename(req.url) + ".html"
+  );
+  console.log(req.url);
+  let extName = path.extname(filePath);
+  let contentType = "text/html";
+
+  switch (extName) {
+    case ".html":
+      contentType = "text/html";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENONET") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf-8");
+          }
+        );
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
